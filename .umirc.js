3febc56264356b36fa988d99a027c929b7c43fe4
zhujixiang <835700239@qq.com>
2022-11-21 23:42:48 +0800

    1
    1

---
+++ .umirc.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "umi"
+
+export default defineConfig({
+    outputPath: 'docs',
+    // history: { type: 'hash' },
+    // hash: true,
+    // exportStatic: {},
+})
+
+// file:///D:/githubcode/zhujixiang.github.io/docs/umi.1e8ad42b.js
+    // publicPath: 'D:/githubcode/zhujixiang.github.io/docs/',
+// file:///D:/umi.1e8ad42b.js
+
+    // base: './',
+    // publicPath: './',
---
+++ docs/src__pages__HomePage__index.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[705],{705:function(){}}]);
