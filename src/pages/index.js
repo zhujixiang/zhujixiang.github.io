f9d51a7ac507cd38f9f82c69a330a89242566be6
zhujixiang <835700239@qq.com>
2022-11-25 00:03:49 +0800

    1
    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import { PureComponent } from "react";
 
+import { Button } from 'antd'
+
 class Index extends PureComponent {
     render() {
         console.log(this)
         return <>
             homepage
+            <Button type="primary">这就很good，let`s begning！！！</Button>
         </>
     }
 }
